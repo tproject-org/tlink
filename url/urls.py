e695324d022afd8a43fcf1ab921f70d49bb6ab70
itotutona <itotutona@evta.fr>
2022-03-29 17:35:30 +0200

    Add urls

---
+++ url/urls.py
@@ -0,0 +1,12 @@
+from .serializers import URLSerializer
+from rest_framework import routers
+from django.urls import path, include
+from .views import URLViewSet
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'url', URLViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
