fccd4fbf02249006a3a4ccf77f6034a0e31251c9
itotutona <itotutona@evta.fr>
2022-03-29 17:48:40 +0200

    Update heritage

--- url/serializers.py
+++ url/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import routers, serializers, viewsets
 from .models import URL
 
 # Serializers define the API representation.
-class URLSerializer(serializers.HyperlinkedModelSerializer):
+class URLSerializer(serializers.ModelSerializer):
     class Meta:
         model = URL
         fields = '__all__'
