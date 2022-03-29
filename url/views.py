ae0551e0329facf0afb75da577124e41972b1cfe
itotutona <itotutona@evta.fr>
2022-03-29 17:35:25 +0200

    Add view sets

--- url/views.py
+++ url/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
 # Create your views here.
+
+from .models import URL
+from .serializers import UrlSerializer
+
+# ViewSets define the view behavior.
+class URLViewSet(viewsets.ModelViewSet):
+    queryset = URL.objects.all()
+    serializer_class = UrlSerializer
+
