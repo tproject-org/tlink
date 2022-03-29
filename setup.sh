f62a46dee4765a1808c58041d7022469101405b3
itotutona <itotutona@evta.fr>
2022-03-29 18:55:55 +0200

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+git clone https://github.com/tproject-org/tlink
+cd tlink
+virtualenv -p python3 tlink-venv
+source tlink-venv/bin/activate
+pip install -r requirements.txt
+
 chmod +x ./manage.py
 
 ./manage.py makemigrations
