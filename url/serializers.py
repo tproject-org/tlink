67b81c0f5a9c29725e74fc06d8d569eab99b2814
itotutona <itotutona@evta.fr>
2022-03-29 17:36:39 +0200

    Capitalize all URL

--- url/serializers.py
+++ url/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import routers, serializers, viewsets
 from .models import URL
 
 # Serializers define the API representation.
-class UrlSerializer(serializers.HyperlinkedModelSerializer):
+class URLSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = URL
         fields = ('url', 'short_url', 'owner')
--- url/views.py
+++ url/views.py
@@ -5,10 +5,10 @@ from rest_framework import viewsets
 # Create your views here.
 
 from .models import URL
-from .serializers import UrlSerializer
+from .serializers import URLSerializer
 
 # ViewSets define the view behavior.
 class URLViewSet(viewsets.ModelViewSet):
     queryset = URL.objects.all()
-    serializer_class = UrlSerializer
+    serializer_class = URLSerializer
 
