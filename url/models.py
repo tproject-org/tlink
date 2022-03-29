3c8d07691259db047a90f79f4860ed472c46201c
itotutona <itotutona@evta.fr>
2022-03-29 17:29:48 +0200

    Create url model

--- url/models.py
+++ url/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class URL(models.Model):
+    url = models.CharField(max_length=1000)
+    short_url = models.CharField(max_length=1000)
+    number_of_view = models.IntegerField(default=0)
+    owner = models.ForeignKey('auth.User', related_name='urls', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.url
