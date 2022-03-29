7dc90cc2c9db56467288e46a9a939d6a3fff388e
itotutona <itotutona@evta.fr>
2022-03-29 17:48:31 +0200

    Remove ununsed argument

--- url/models.py
+++ url/models.py
@@ -6,7 +6,7 @@ class URL(models.Model):
     url = models.CharField(max_length=1000)
     short_url = models.CharField(max_length=1000)
     number_of_view = models.IntegerField(default=0)
-    owner = models.ForeignKey('auth.User', related_name='urls', on_delete=models.CASCADE)
-    
+    owner = models.ForeignKey('auth.User',  on_delete=models.CASCADE)
+
     def __str__(self):
         return self.url
