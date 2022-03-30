1fb8b46bc765815d9397444e2717c1f9c984054e
itotutona <itotutona@evta.fr>
2022-03-30 16:50:55 +0200

    Addd unique

--- url/models.py
+++ url/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class URL(models.Model):
-    url = models.URLField(max_length=1000)
+    url = models.URLField(max_length=1000, unique=True)
     short_url = models.CharField(max_length=1000, blank=True)
     number_of_view = models.IntegerField(default=0)
     owner = models.ForeignKey('auth.User',  on_delete=models.CASCADE)
